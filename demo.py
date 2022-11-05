f2828582de43293bdb55623b9039c764229ce730
debbie <debbie.dy.xie@gmail.com>
2022-11-06 00:27:25 +0800

    url

--- demo.py
+++ demo.py
@@ -11,7 +11,7 @@ driver = webdriver.Remote(
     desired_capabilities=DesiredCapabilities.CHROME
 )
 
-driver.get("https://www.baidu.com")
+driver.get("https://www.shopperplus.ca")
 content = driver.title.split("_")[0]
 print(content)
 driver.close()
